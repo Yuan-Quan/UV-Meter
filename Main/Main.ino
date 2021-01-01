c7d234c9093e03de7418424e291a5bf9d28446b5
Luna Child <metroyuan@gmail.com>
2021-01-02 01:51:25 +0800

    defined LEDs

--- Main/Main.ino
+++ Main/Main.ino
@@ -11,6 +11,23 @@
 #define BRIGHTNESS_MIN 0  //The minimum grascale value globally
 #define BRIGHTNESS_MAX 255  //The maximum grascale value globally
 
+/*Corresponding index for led on panle
+*/
+#define LED_INPUT_SELECTOR 0
+#define LED_14INCH 1
+#define LED_XLR 2
+#define LED_GAIN_CONTROL 3
+#define LED_GAIN_CH1 4
+#define LED_GAIN_CH2 5
+#define LED_LINK 6
+#define LED_MASTERL 7
+#define LED_SLAVEL 8
+#define LED_MASTERR 9
+#define LED_SLAVER 10
+#define LED_CLIP_INDICATOR 11
+#define LED_CLIP_CH1 12
+#define LED_CLIP_CH2 13
+
 void setup()
 {
   // Call Tlc.init() to setup the tlc.
