38515c4eebe8c3da970382556f0b5d359236adde
Luna Child <metroyuan@gmail.com>
2021-01-02 01:42:49 +0800

    implemented: GetGrayscaleValue(uint8_t value)

--- Main/Main.ino
+++ Main/Main.ino
@@ -6,6 +6,7 @@
 /*Range of LED brightness, 
   RGB value form 0-255 will be maped to this, 
   then parse to TLC
+  (0-4095)
 */
 #define BRIGHTNESS_MIN 0  //The minimum grascale value globally
 #define BRIGHTNESS_MAX 255  //The maximum grascale value globally
@@ -19,4 +20,11 @@ void setup()
 void loop()
 {
   
+}
+
+///////////Utility Methods/////////////
+
+int GetGrayscaleValue(uint8_t value)
+{
+  return map(value, 0, 255, BRIGHTNESS_MIN, BRIGHTNESS_MAX);
 }
